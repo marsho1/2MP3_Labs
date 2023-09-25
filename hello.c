772997d728bfef4c00e670565dc48c135c4be129
marsho1 <marsho1@mcmaster.ca>
2023-09-25 10:08:51 -0400

    3rd commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+//this code is written by Pedram
+#include <stdio.h>
+//the main function
+int main (void) {
+    /* calling " printf " function
+    the " defination " is included in " stdio " library */
+    printf("Hello , McMaster!\n");
+}
+//im typing some things
